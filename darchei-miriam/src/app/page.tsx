2dfd6792f3bf8ec56f616fc3bcbbd6e7587391d3
meirfisher <mfr5995@gmail.com>
2023-07-19 14:51:59 +0300

    new home page

--- darchei-miriam/src/app/page.tsx
+++ darchei-miriam/src/app/page.tsx
@@ -1,3 +1,21 @@
+import Link from 'next/link';
+import styles from './page.module.css'
+
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
+  return (
+    <div className={styles.page}>
+      <h1 className={styles.headline}>טיוטת מערכת דרכי מרים</h1>
+      <div className={styles.buttonContainer}>
+        <Link href="/Add-ride" passHref>
+          <button className={styles.button}>הוספת נסיעה חדשה</button>
+        </Link>
+        <Link href="/open-rides" passHref>
+          <button className={styles.button}>נסיעות פתוחות</button>
+        </Link>
+        <Link href="" passHref>
+          <button className={styles.button}>נסיעות סגורות</button>
+        </Link>
+      </div>
+    </div>
+  );
 }
